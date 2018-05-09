e5ae816a4325508f588486cec0abf18b4ed1c233
snoyes <snoyes@OVDIMC10-358V.coast.cccd.adx>
2018-05-08 19:47:14 -0700

    Update 1

--- IC13_VGInventory/src/edu/orangecoastcollege/cs272/ic13/view/VideoGamesListScene.java
+++ IC13_VGInventory/src/edu/orangecoastcollege/cs272/ic13/view/VideoGamesListScene.java
@@ -20,24 +20,37 @@ public class VideoGamesListScene implements Initializable {
 	private ComboBox<String> publishersCB;
 	@FXML
 	private ComboBox<String> platformsCB;
-	
+
 
 	@Override
 	public void initialize(URL location, ResourceBundle resources) {
-		//TODO: Complete this method
+	 // TODO: Override the initialize method (let Eclipse generate the method for you)
+	    // In this method:
+	    // 1) Set the items of the allVideoGamesLV to all video games from the controller
+	    allVideoGamesLV.setItems(controller.getAllVideoGames());
+	    // 2) Set the items of the platformsCB to the distinct platforms from the controller
+	    platformsCB.setItems(controller.getDistinctPlatforms());
+	    // 3) Set the items of the publishersCB to the distinct publishers from the controller
+	    publishersCB.setItems(controller.getDistinctPublishers());
 	}
-	
+
 	@FXML
 	public Object addGameToInventory()
 	{
-		//TODO: Complete this method
-		return this;
+	  //TODO: Complete this method
+        // 1) Get the selected video game from the allVideoGamesLV (use getSelectedItem())
+        VideoGame selectedGame = allVideoGamesLV.getSelectionModel().getSelectedItem();
+	    // 2) Use the controller to add the selected game to the inventory
+        return controller.addGameToUsersInventory(selectedGame);
+	    // 3) Return the result (as a boolean)
+
 	}
-	
+
 	@FXML
 	public Object viewInventory()
 	{
 		//TODO: Complete this method
+	    ViewNavigator.loadScene("User's Video Games", ViewNavigator.VIEW_INVENTORY_SCENE);
 		return this;
 	}
 
